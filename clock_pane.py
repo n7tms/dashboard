da21f5d7e394f0c748e9996b5dc4eaa2180ad7ca
n7tms@yahoo.com <n7tms@yahoo.com>
2023-01-23 17:03:59 -0700

    icon (fail)

--- clock_pane.py
+++ clock_pane.py
@@ -9,5 +9,32 @@
 # https://www.plus2net.com/python/tkinter-analog-clock-dial.php
 #
 #
+import tkinter as tk
+from datetime import datetime
 
+def clock_panel(f:tk.Frame) -> tk.Frame:
+
+    def update_clock():
+        """Update the clock every second."""
+        now = datetime.now()
+        current_time = now.strftime("%H:%M:%S")
+
+        lbllocaltime.configure(text=current_time)
+        lblUTCtime.configure(text=current_time)
+        f.after(1000,update_clock)
+
+
+    lblLocal = tk.Label(f,text="Local:")
+    lblLocal.grid(row=0,column=0,padx=5,pady=20)
+    lbllocaltime = tk.Label(f,text="")
+    lbllocaltime.grid(row=0,column=1,padx=5,pady=20)
+
+    lblUTC = tk.Label(f,text="UTC:")
+    lblUTC.grid(row=1,column=0,padx=5,pady=20)
+    lblUTCtime = tk.Label(f,text="")
+    lblUTCtime.grid(row=1,column=1,padx=5,pady=20)
+
+
+    update_clock()
+    return f
 
--- dashboard.py
+++ dashboard.py
@@ -1,3 +1,31 @@
 # dashboard.py
 #
 # main dashboard screen
+
+import tkinter as tk
+import clock_pane as cp
+
+dashboard = tk.Tk()
+dashboard.title("Dashboard")
+
+# pi = tk.PhotoImage(file='phone_icon.png')
+dashboard.iconphoto(False,tk.PhotoImage(file='phone_icon.png'))
+# dashboard.tk.call('wm','iconphoto',dashboard._w, tk.PhotoImage(file='dashboard_logo.png'))
+
+
+
+frame = tk.Frame(dashboard)
+frame.pack()
+
+# Clock
+frmClock = tk.LabelFrame(frame,text="Clock")
+frmClock.grid(row=0,column=0,padx=20,pady=20)
+cp.clock_panel(frame)
+
+# Weather
+
+
+# Calendar
+
+
+dashboard.mainloop()
